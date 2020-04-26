d788011bf151bbe021dedb6fa7e8227b8aae6cc7
nick-k-dev <20979916+nick-k-dev@users.noreply.github.com>
2020-04-26 17:43:25 -0400

    Initial page setup completed with bare minimum displaying of data

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+    bio = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=500)
+    created_date = models.DateTimeField('date created')
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
 
-# Create your views here.
+from .models import Post, Author
+
+class IndexView(generic.ListView):
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
+    
+    def get_queryset(self):
+        return Post.objects.all()
+
+class DetailView(generic.DetailView):
+    model = Post
+    template_name = 'blog/detail.html'
