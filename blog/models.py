b7219861ca6eaf18fecf5061d186a3cba235d207
nick-k-dev <20979916+nick-k-dev@users.noreply.github.com>
2020-04-27 11:53:59 -0400

    Added static folder to set up css file and add some placeholder images

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=50)
     bio = models.CharField(max_length=300)
+    img_url = models.TextField(blank=True)
 
     def __str__(self):
         return self.name
